ab7bb63eb330f0378d03302e0713154bde494ecc
yubang <yubang93@gmail.com>
2017-04-11 19:24:45 +0800

    init

---
+++ config.py
@@ -0,0 +1,15 @@
+# coding:UTF-8
+
+# 调用API的时候需要提供的信令
+TOKEN = 'abc'
+
+# WEB服务相关
+WEB_LISTEN_IP = '127.0.0.1'
+WEB_LISTEN_PORT = 8000
+
+# 存储设置
+storage = {
+    "type": 'leancloud',
+    "API_ID": '',
+    "API_KEY": '',
+}
---
+++ web.py
@@ -0,0 +1,9 @@
+# coding:UTF-8
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
